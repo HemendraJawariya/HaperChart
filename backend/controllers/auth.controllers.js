c4a59368567188b57f7b781147c3a78c34800d06
Hemendra Jawariya <hemendrajawariya@gmail.com>
2025-08-12 11:19:32 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -25,8 +25,8 @@ export const signUp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      secure: false,
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "None",
     })
 
     return res.status(201).json(user)
@@ -49,8 +49,8 @@ export const signIn = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      secure: false,
-      sameSite: "Strict",
+     secure: true,
+      sameSite: "None",
     })
 
   
@@ -153,3 +153,4 @@ export const deleteStory = async (req, res) => {
     return res.status(500).json({ message: "Story delete error" })
   }
 }
+
